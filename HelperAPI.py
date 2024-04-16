edb6817fb8df076b54dc26200baa2971d9d2f229
VladimirBerg <vovabergg@gmail.com>
2024-04-16 12:01:57 +0300

    Предварительный коммит

--- HelperAPI.py
+++ HelperAPI.py
@@ -1,6 +1,9 @@
+import allure
+
 from ModelAPI import JsTestTask
 
 
+@allure.step("Get list of products")
 def js_test_task(api_client):
     url = "https://www.lenvendo.ru/api/js-test-task/"
     products_list = []
@@ -11,22 +14,25 @@ def js_test_task(api_client):
     data = response.json()
 
     while data['next_page_url'] is not False:
-        for item in data['products']:
-            print(item)
-            products_list.append(item)
-        page += 1
-        get_params['page'] = str(page)
-        response = api_client.get(url, params=get_params)
-        data = response.json()
+        with allure.step(f"Get list from {page} page"):
+            for item in data['products']:
+                print(item)
+                products_list.append(item)
+            page += 1
+            get_params['page'] = str(page)
+            response = api_client.get(url, params=get_params)
+            data = response.json()
     if data['next_page_url'] == False:
-        for item in data['products']:
-            products_list.append(item)
+        with allure.step(f"Get list from {page} page"):
+            for item in data['products']:
+                products_list.append(item)
 
     print('@products_list@', products_list)
     objects = [JsTestTask(**item) for item in products_list]
     return objects
 
 
+@allure.step("Check that each name include 'Alcatel'")
 def check_string_in_names(list_of_names):
     a = True
     for item in list_of_names:
--- Tests.py
+++ Tests.py
@@ -4,21 +4,27 @@ import time
 import allure
 
 
-@allure.title("click_value_of_cards")
+@allure.title("Check active and selected card value in input-field")
 def test_click_value_of_cards(browser):
     main_page = CardDenominations(browser)
-    main_page.open_base_page()
+    with allure.step("Open base page"):
+        main_page.open_base_page()
     time.sleep(2)
     list_of_denominations = main_page.go_to_card_denominations()
-    for x in range(0, len(list_of_denominations)):
-        active, text = main_page.check_value_and_active(list_of_denominations, x)
-        assert active == True
-        assert text == main_page.get_value_from_field()
+    with allure.step(f"Checking"):
+        for x in range(0, len(list_of_denominations)):
+            active, text = main_page.check_value_and_active(list_of_denominations, x)
+            with allure.step(f"Check active: {text}"):
+                assert active == True
+            with allure.step(f"Check selected card value in input-field: {main_page.get_value_from_field()}"):
+                assert text == main_page.get_value_from_field()
 
 
 @allure.title("Test API")
 def test_get_todo(api_client):
     list_of_names = HelperAPI.js_test_task(api_client)
     a = HelperAPI.check_string_in_names(list_of_names)
-    assert a == True
-    assert sorted(list_of_names) == list_of_names
+    with allure.step("All names include 'Alcatel'"):
+        assert a == True
+    with allure.step("Right sorting"):
+        assert sorted(list_of_names) == list_of_names
